d640f4f96d961eded9eefcdde5de7d6c343a345a
Robert Mielewczyk <rob.mielewczyk@gmail.com>
2020-06-18 22:57:23 +0200

    Added Error Handling

--- src/app/timeapi.service.ts
+++ src/app/timeapi.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
+import {  throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,23 @@ export class TimeapiService {
     private http: HttpClient
   ) { }
 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
   getCurrentTime(timezone: string){
-    return this.http.get(`http://worldtimeapi.org/api/timezone/${timezone}`);
+    return this.http.get(`http://worldtimeapi.org/api/timezone/${timezone}`)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 }
